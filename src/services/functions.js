3d9c410da6dc6ce5b88412250e596c822ee7de87
James Caroe <jamescaroe@gmail.com>
2025-02-11 13:56:41 +0000

    Removed logic error

--- src/services/functions.js
+++ src/services/functions.js
@@ -1,7 +1,7 @@
 export const fetchAPI = async (url, params) => {
   const headers = {
     ...(params.headers || {}),
-    'Authorization': `Bearer ${import.meta.env.VITE_API_KEY}`
+    'Authorization': `${import.meta.env.VITE_API_KEY}`
   };
 
   return fetch(url, {
