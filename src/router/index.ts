868a894ac7f4821c53950743677b80f3220f6f5f
James Caroe <jamescaroe@gmail.com>
2025-02-12 07:09:51 +0000

    Update redirect logic

--- src/router/index.ts
+++ src/router/index.ts
@@ -46,7 +46,7 @@ router.beforeEach((to, from, next) => {
     to.path.startsWith('/editEvent') ||
     to.path.startsWith('/templateEvents') ||
     to.path.startsWith('/users'))) {
-    next(`/login`);
+    next(`/login?redirect=${to.path}`);
   }
   else {
     next();
