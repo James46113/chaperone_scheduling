23ffdc07d62c81124ea4b0c9fad16c3bb6623e33
James Caroe <jamescaroe@gmail.com>
2025-02-18 18:03:42 +0000

    updateURL

--- src/services/functions.js
+++ src/services/functions.js
@@ -5,7 +5,7 @@ export const fetchAPI = async (url, params) => {
   };
 
   const APIURL = window.location.href.startsWith('https://chaperonescheduling-dev.up.railway.app')
-    ? 'chaperone_scheduling_api.railway.internal/' : import.meta.env.VITE_API_URL;
+    ? 'https://chaperone_scheduling_api.railway.internal/' : import.meta.env.VITE_API_URL;
 
   return fetch(APIURL + url, {
     ...params,
