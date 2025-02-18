edf2f81634b8236df93c851724362c7d5f2cbd9e
James Caroe <jamescaroe@gmail.com>
2025-02-18 19:28:20 +0000

    module syntax

--- public/server.js
+++ public/server.js
@@ -1,5 +1,6 @@
-const express = require('express');
-const fetch = require('node-fetch');
+import express from 'express';
+import fetch from 'node-fetch';
+
 const app = express();
 const port = 3000;
 
@@ -9,7 +10,7 @@ app.use('/api', async (req, res) => {
   const url = `https://chaperone_scheduling_api.railway.internal:5000/${req.url}`;
   const headers = {
     ...req.headers,
-    'Authorization': `Bearer dZtMYO-zQyCeh-a48QkX-Pztgx9-LyDAXG-GMgj7a`
+    'Authorization': `Bearer ${process.env.API_KEY}`
   };
 
   try {
