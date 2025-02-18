d024a3ae3d3385286ce37845320f32b01602f980
James Caroe <jamescaroe@gmail.com>
2025-02-18 09:58:34 +0000

    API url for dev

--- src/services/functions.js
+++ src/services/functions.js
@@ -1,7 +1,7 @@
 export const fetchAPI = async (url, params) => {
   const headers = {
     ...(params.headers || {}),
-    'Authorization': `${import.meta.env.VITE_API_KEY}`
+    'Authorization': `${isDev.value ? import.meta.env.VITE_API_KEY_DEV : import.meta.env.VITE_API_KEY}`
   };
 
   return fetch(import.meta.env.VITE_API_URL + url, {
