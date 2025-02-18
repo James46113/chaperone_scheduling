61d784aa41368df150134004bef2fb5397d70233
James Caroe <jamescaroe@gmail.com>
2025-02-18 13:52:24 +0000

    debugging

--- src/services/functions.js
+++ src/services/functions.js
@@ -3,6 +3,7 @@ export const fetchAPI = async (url, params) => {
     ...(params.headers || {}),
     'Authorization': `${import.meta.env.VITE_DEV == 1 ? import.meta.env.VITE_API_KEY_DEV : import.meta.env.VITE_API_KEY}`
   };
+  console.log(import.meta.env.VITE_DEV == 1, import.meta.env.VITE_API_KEY_DEV, import.meta.env.VITE_API_KEY)
 
   return fetch(import.meta.env.VITE_API_URL + url, {
     ...params,
