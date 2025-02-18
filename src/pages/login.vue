164bdf68af21bdb33d845d3adcb9a84a942368e0
James Caroe <jamescaroe@gmail.com>
2025-02-18 20:27:20 +0000

    revert

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -45,8 +45,7 @@ function onSignIn(response) {
   loadingData.value = true;
   Cookies.set('credential', response.credential);
   store.userEmail = decodeCredential(response.credential).email;
-  const idToken = response.getAuthResponse().id_token;
-  console.log(idToken)
+
 
   fetchAPI(`login/${store.userEmail}`, {
     method: 'GET',
