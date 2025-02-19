55e4d6378e6e5a173ce622f77947557a4ae85b5e
James Caroe <jamescaroe@gmail.com>
2025-02-19 09:22:31 +0000

    auth update

--- public/server.js
+++ public/server.js
@@ -8,58 +8,6 @@ const client = new OAuth2Client('898082729738-m1b4g6ls0l88lvosj3pb79ki7buid87p.a
 
 app.use(express.json());
 
-app.use('/api', async (req, res) => {
-  const url = `http://chaperone_scheduling_api.railway.internal:5000`;
-  let payload = null;
-  try {
-
-    const idToken = req.headers.authorization?.split(' ')[1];
-
-    if (!idToken) {
-      return res.status(401).json({ error: 'Unauthorized' });
-    }
-
-    const ticket = await client.verifyIdToken({
-      idToken,
-      audience: '898082729738-m1b4g6ls0l88lvosj3pb79ki7buid87p.apps.googleusercontent.com',
-    });
-
-    payload = ticket.getPayload();
-
-    if (!payload) {
-      return res.status(401).json({ error: 'Unauthorized' });
-    }
-
-    await fetch(`${url}/email/verify/${payload.email}`, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-      }
-    }).then((response) => {
-      if (!response.ok) {
-        return res.status(401).json({ error: 'Unauthorized' });
-      }
-    });
-
-  } catch (error) {
-    console.error('Error:', error);
-    return res.status(401).json({ error: 'Unauthorized' });
-  }
-
-  try {
-    const response = await fetch(`${url}${req.url}`, {
-      method: req.method,
-      headers: { ...req.headers, email: payload.email },
-      body: req.method !== 'GET' ? JSON.stringify(req.body) : undefined
-    });
-    const data = await response.json();
-    res.status(response.status).json(data);
-  } catch (error) {
-    console.error('Error:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
-});
-
 
 app.post('/api/token', async (req, res) => {
   const { code } = req.body;
@@ -80,6 +28,7 @@ app.post('/api/token', async (req, res) => {
 
   const data = await response.json();
   if (data.error) {
+    console.log(data.error);
     return res.status(400).json({ error: data.error });
   }
 
@@ -118,7 +67,64 @@ app.post('/api/refresh-token', async (req, res) => {
   });
 });
 
+async function verifyAccessToken(accessToken) {
+  const response = await fetch(`https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`);
+  const data = await response.json();
+  if (data.error) {
+    throw new Error(data.error);
+  }
+  return data;
+}
 
+app.use('/api', async (req, res) => {
+  const url = `http://chaperone_scheduling_api.railway.internal:5000`;
+  let payload = null;
+  try {
+    const accessToken = req.headers.authorization?.split(' ')[1];
+
+    if (!accessToken) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const tokenInfo = await verifyAccessToken(accessToken);
+
+    if (!tokenInfo) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    if (tokenInfo.audience !== '898082729738-m1b4g6ls0l88lvosj3pb79ki7buid87p.apps.googleusercontent.com') {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    await fetch(`${url}/email/verify/${tokenInfo.email}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    }).then((response) => {
+      if (!response.ok) {
+        return res.status(401).json({ error: 'Unauthorized' });
+      }
+    });
+
+  } catch (error) {
+    console.error('Error:', error);
+    return res.status(401).json({ error: 'Unauthorized' });
+  }
+
+  try {
+    const response = await fetch(`${url}${req.url}`, {
+      method: req.method,
+      headers: { ...req.headers, email: payload.email },
+      body: req.method !== 'GET' ? JSON.stringify(req.body) : undefined
+    });
+    const data = await response.json();
+    res.status(response.status).json(data);
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
