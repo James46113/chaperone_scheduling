20b3f35fdc42b05aaf3174d4936e1d7f1a8ee552
James Caroe <jamescaroe@gmail.com>
2025-02-22 14:34:06 +0000

    Bug fix

--- src/router/index.ts
+++ src/router/index.ts
@@ -39,7 +39,6 @@ router.afterEach((to, from) => window.scrollTo(0, 0))
 
 router.beforeEach((to, from, next) => {
   const store = useAppStore();
-  if (window.location.hostname === 'localhost') next();
   if (!isSignedIn.value && to.path !== '/login' && to.path !== '/resetPassword') {
     if ((Cookies.get('credential') && Cookies.get('accessToken') && Cookies.get('refreshToken')) || Cookies.get('passwdAccessToken')) {
       next();
