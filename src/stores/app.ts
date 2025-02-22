7af478d58b4b51091aa7008bb2d36319ca76b020
James Caroe <jamescaroe@gmail.com>
2025-02-22 14:48:23 +0000

    update

--- src/stores/app.ts
+++ src/stores/app.ts
@@ -42,7 +42,7 @@ export const useAppStore = defineStore('app', () => {
       event.end = new Date(event.end);
       event.date = new Date(event.start);
       event.slots = computed(() => chaperoneSlots.value.filter((slot: any) => slot.event_id === event.id));
-      event.available = computed(() => availability.value.filter((avail: any) => avail.event_id === event.id).map((avail: any) => avail.availabile));
+      event.available = computed(() => availability.value.filter((avail: any) => avail.event_id === event.id).map((avail: any) => avail.availabile)[0]);
 
       event.chaperones = computed(() => {
         const slots = chaperoneSlots.value.filter((slot: any) => slot.event_id === event.id)
