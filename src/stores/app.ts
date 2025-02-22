1f204880a1ae525459129a377764c6c2231d324d
James Caroe <jamescaroe@gmail.com>
2025-02-22 15:20:49 +0000

    yet more fixing

--- src/stores/app.ts
+++ src/stores/app.ts
@@ -147,10 +147,6 @@ export const useAppStore = defineStore('app', () => {
     return events.value.find((event: any) => event.id === id);
   }
 
-  const getEventAvailability = (id: number) => {
-    return availability.value.find((avail: any) => avail.event_id === id);
-  }
-
 
   return {
     // GENERAL
