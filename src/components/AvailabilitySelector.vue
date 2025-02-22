ba1247175b9b6f082d7b7587db1161b28385ca9e
James Caroe <jamescaroe@gmail.com>
2025-02-22 14:43:11 +0000

    Update

--- src/components/AvailabilitySelector.vue
+++ src/components/AvailabilitySelector.vue
@@ -5,11 +5,11 @@
     <v-card-title v-else class="mb-3">Available:</v-card-title>
     <v-row class="mx-2 mb-3">
       <v-btn variant="flat" max-width="100px" width="50%"
-        :color="event.available === null ? '' : event.available ? '#198754' : ''" @click="updateAvailable(true)"
-        :ripple="false">✓</v-btn>
+        :color="store.getEvent(event).available === null ? '' : store.getEvent(event).available ? '#198754' : ''"
+        @click="updateAvailable(true)" :ripple="false">✓</v-btn>
       <v-btn variant="flat" max-width="100px" width="50%"
-        :color="event.available === null ? '' : event.available ? '' : 'primary'" @click="updateAvailable(false)"
-        :ripple="false">⨯</v-btn>
+        :color="store.getEvent(event).available === null ? '' : store.getEvent(event).available ? '' : 'primary'"
+        @click="updateAvailable(false)" :ripple="false">⨯</v-btn>
     </v-row>
   </div>
 </template>
@@ -19,17 +19,18 @@ import { useAppStore } from '@/stores/app';
 
 
 const props = defineProps({
-  event: Object,
+  event: Number,
   small: Boolean
 })
 
 const store = useAppStore();
-const isPastEvent = computed(() => props.event.date < new Date())
+const isPastEvent = computed(() => store.getEvent(props.event).date < new Date())
 
 
 const updateAvailable = (availability) => {
-  const originalAvailability = props.event.available
-  props.event.available = availability
+  const originalAvailability = store.getEvent(props.event).available
+  store.getEvent(props.event).available = availability
+
   fetchAPI(`chaperones/availability/${store.userID}`, {
     // fetchAPI(`chaperones/availability/1`, { // DEBUG ONLY
     method: 'PATCH',
@@ -37,17 +38,17 @@ const updateAvailable = (availability) => {
       'Content-Type': 'application/json',
     },
     body: JSON.stringify({
-      event_id: props.event.id,
+      event_id: props.event,
       available: availability
     })
   })
     .then((response) => {
       if (response.status === 403) {
         store.showAlert('Assigned', 'You are already assigned to this event, please contact Angela on +44 7985 925570 if you are no longer available.')
-        props.event.available = originalAvailability
+        store.getEvent(props.event).available = originalAvailability
       } else if (!response.ok) {
         store.showAlert('Failed to update availability', 'Are you connected to the internet?')
-        props.event.available = originalAvailability
+        props.getEvent(props.event).available = originalAvailability
       }
     })
 }
