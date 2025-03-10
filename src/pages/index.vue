b123e27312b39d9fa6a4f9598051c3b57f6e908b
James Caroe <jamescaroe@gmail.com>
2025-03-10 22:17:33 +0000

    Remove availability selector for choir phone

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -76,7 +76,7 @@ const store = useAppStore();
 document.title = "Chaperones' Calendar - Steel City Choristers"
 
 onMounted(async () => {
-  if (store.userID == 10) store.tabView = 'list';
+  if (store.isChoirPhone == 10) store.tabView = 'list';
   if (proxy.$route.query.view) {
     store.tabView = proxy.$route.query.view;
   }
