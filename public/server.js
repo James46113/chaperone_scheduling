7d69f95d71890bfb4c54018465e2b2ce3c9d0f2c
James Caroe <jamescaroe@gmail.com>
2025-03-11 20:04:23 +0000

    debug

--- public/server.js
+++ public/server.js
@@ -108,8 +108,12 @@ app.use('/api/public/', async (req, res) => {
     headers: req.headers,
     body: req.method !== 'GET' ? JSON.stringify(req.body) : undefined
   });
-
-  return res.status(response.status).json(await response.json());
+  try {
+    return res.status(response.status).json(await response.json());
+  }
+  catch {
+    return res.status(response.status).send(response.clone().text());
+  }
 });
 
 
@@ -133,7 +137,6 @@ app.use('/api/p/', async (req, res) => {
     headers: { ...req.headers, email: email },
     body: req.method !== 'GET' ? JSON.stringify(req.body) : undefined
   })
-  console.log('request failed')
   return res.status(response.status).json(await response.json());
 });
 
