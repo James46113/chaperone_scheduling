50be5087a57cccd5867707eac27060fc69b69ea8
James Caroe <83098125+James46113@users.noreply.github.com>
2025-03-12 07:04:34 +0000

    Revert "Added drag and drop chaperone editing"

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -67,6 +67,7 @@ import { VCalendar } from 'vuetify/labs/VCalendar'
 import { ref, onMounted, getCurrentInstance } from 'vue'
 import { useAppStore } from '@/stores/app'
 
+const events = ref([])
 
 const { proxy } = getCurrentInstance()
 
