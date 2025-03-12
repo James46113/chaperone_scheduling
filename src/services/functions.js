32de6e70e8cd58056da03db64292307b8c2b5d77
James Caroe <jamescaroe@gmail.com>
2025-03-12 07:11:46 +0000

    bug fix

--- src/services/functions.js
+++ src/services/functions.js
@@ -11,22 +11,20 @@ export const fetchAPI = async (url, params) => {
   // const APIURL = window.location.href.startsWith('https://chaperonescheduling-dev.up.railway.app')
   //   ? 'https://chaperone_scheduling_api.railway.internal:5000/' : import.meta.env.VITE_API_URL;
   let APIURL;
-  if (!isSignedIn.value) {
-    APIURL = "";
-  } else if (usingPasswordLogin.value) {
+  if (usingPasswordLogin.value) {
     APIURL = '/api/p/';
   } else {
     APIURL = '/api/'
   }
 
-  let HOSTNAME;
-  if (window.location.hostname === 'localhost') {
-    HOSTNAME = 'https://dev-chaperones-steelcitychoristers.up.railway.app';
-  } else {
-    HOSTNAME = ''
-  }
+  // let HOSTNAME;
+  // if (window.location.hostname === 'localhost') {
+  //   HOSTNAME = 'https://dev-chaperones-steelcitychoristers.up.railway.app';
+  // } else {
+  //   HOSTNAME = ''
+  // }
 
-  return fetch(HOSTNAME + APIURL + url, {
+  return fetch(APIURL + url, {
     ...params,
     headers
   }).then((response) => {
