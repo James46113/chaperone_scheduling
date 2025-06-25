30bd64bbabaf3b7550bd4f441b8ef9599dc84094
James Caroe <jamescaroe@gmail.com>
2025-06-25 15:21:50 +0100

    update notificaiton permissions

--- src/services/functions.js
+++ src/services/functions.js
@@ -86,10 +86,31 @@ window.addEventListener('offline', () => {
   }
 });
 
+
+function urlBase64ToUint8Array(base64String) {
+  const padding = '='.repeat((4 - base64String.length % 4) % 4);
+  const base64 = (base64String + padding)
+    .replace(/-/g, '+')
+    .replace(/_/g, '/');
+  const rawData = window.atob(base64);
+  const outputArray = new Uint8Array(rawData.length);
+  for (let i = 0; i < rawData.length; ++i) {
+    outputArray[i] = rawData.charCodeAt(i);
+  }
+  return outputArray;
+}
+
+
 export const notificationsSubscribe = async (userID) => {
+      const permission = await Notification.requestPermission();
+      if (permission !== 'granted') {
+        alert('Notifications are not enabled.');
+        return;
+      }
+
       const subscription = await serviceworker.value.pushManager.subscribe({
         userVisibleOnly: true,
-        applicationServerKey: 'BDxiXIhjftxcAEVif8XED2Qah1j6FFZUO8iN2GFDK_aadrb2ad23Z_i85whgM2TIJ3uKGXntjTbc0m8C-4r4pjc'
+        applicationServerKey: urlBase64ToUint8Array('BDxiXIhjftxcAEVif8XED2Qah1j6FFZUO8iN2GFDK_aadrb2ad23Z_i85whgM2TIJ3uKGXntjTbc0m8C-4r4pjc')
       });
 
       await fetchAPI(`notifications/subscribe/${userID}`, {
